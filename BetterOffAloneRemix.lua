14b9e43aa5ddc78ae11e794e9265e81ee9c46baa
SkidMaster999 <skidmaster999@gmail.com>
2025-07-08 21:20:33 +0800

    Create BetterOffAloneRemix.lua

---
+++ BetterOffAloneRemix.lua
@@ -0,0 +1,58 @@
+local Sound0 = Instance.new("Sound")
+local Sound2 = Instance.new("Sound")
+local DistortionSoundEffect3 = Instance.new("DistortionSoundEffect")
+local Sound4 = Instance.new("Sound")
+local DistortionSoundEffect5 = Instance.new("DistortionSoundEffect")
+local Sound6 = Instance.new("Sound")
+local DistortionSoundEffect7 = Instance.new("DistortionSoundEffect")
+local Sound8 = Instance.new("Sound")
+DistortionSoundEffect9 = Instance.new("DistortionSoundEffect")
+Sound0.Name = "Better Off Alone Mr Brabs - Part 1"
+Sound0.Parent = workspace
+Sound0.Pitch = 0.20000000298023224
+Sound0.PlaybackSpeed = 0.20000000298023224
+Sound0.SoundId = "rbxassetid://139607639508776"
+Sound0.Volume = 10
+Sound2.Name = "Better Off Alone Mr Brabs - Part 2"
+Sound2.Parent = workspace
+Sound2.Pitch = 0.20000000298023224
+Sound2.PlaybackSpeed = 0.20000000298023224
+Sound2.SoundId = "rbxassetid://88867836391089"
+DistortionSoundEffect3.Parent = Sound2
+DistortionSoundEffect3.Level = 0.9900000095367432
+Sound4.Name = "Better Off Alone Mr Brabs - Part 3"
+Sound4.Parent = workspace
+Sound4.Pitch = 0.20000000298023224
+Sound4.PlaybackSpeed = 0.20000000298023224
+Sound4.SoundId = "rbxassetid://75798139272905"
+DistortionSoundEffect5.Parent = Sound4
+DistortionSoundEffect5.Level = 0.9900000095367432
+Sound6.Name = "Better Off Alone Mr Brabs - Part 4"
+Sound6.Parent = workspace
+Sound6.Pitch = 0.20000000298023224
+Sound6.PlaybackSpeed = 0.20000000298023224
+Sound6.SoundId = "rbxassetid://122471915694770"
+DistortionSoundEffect7.Parent = Sound6
+DistortionSoundEffect7.Level = 0.9900000095367432
+Sound8.Name = "Better Off Alone Mr Brabs - Part 5"
+Sound8.Parent = workspace
+Sound8.Pitch = 0.20000000298023224
+Sound8.PlaybackSpeed = 0.20000000298023224
+Sound8.SoundId = "rbxassetid://80673970785240"
+DistortionSoundEffect9.Parent = Sound8
+DistortionSoundEffect9.Level = 0.9900000095367432
+Sound0:Play()
+Sound0.Ended:Wait()
+Sound2:Play()
+Sound2.Ended:Wait()
+Sound4:Play()
+Sound4.Ended:Wait()
+Sound6:Play()
+Sound6.Ended:Wait()
+Sound8:Play()
+Sound8.Ended:Wait()
+Sound0:Destroy()
+Sound2:Destroy()
+Sound4:Destroy()
+Sound6:Destroy()
+Sound8:Destroy()
